f246ee3f5914128797d4f2cd1b0d6e8dadd35539
LuoYuY <15868808504@163.com>
2020-09-21 22:56:43 +0800

    init the project

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Banner from '@/components/Banner'
+import Banner from '../components/Banner'
+import HelloWorld from '../components/HelloWorld'
 
 Vue.use(Router)
 
@@ -12,9 +13,14 @@ export default new Router({
     //   component: HelloWorld
     // },
     {
-      path: '/',
+      path: '/index',
       name: 'Banner',
       component: Banner
+    },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
